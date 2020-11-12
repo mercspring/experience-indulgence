2f5750f606a02beb3149bec397105cdeee860e7a
mercspring <68706508+mercspring@users.noreply.github.com>
2020-11-11 20:41:01 -0800

    added files

---
+++ pages/chef-signup/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+function signup() {
+  const [search, setSearch] = useState("Wikipedia");
+
+  useEffect(() => {
+
+  }, []);
+
+
+  return (
+    <div>
+    </div>
+  );
+}
