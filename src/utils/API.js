ba521d6b5b4d287d4fad1e1e143090bc16ff15b6
Thomas An <tommyan97@msn.com>
2020-11-16 13:09:40 -0800

    fixed api url

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = {
-    api : "https://fast-waters-05843.herokuapp.com/",
+    api : "https://fast-waters-05843.herokuapp.com",
     getAllCuisines: () => axios.get(this.api + "/api/cuisine"),
     getAllSpecialties: () => axios.get(this.api + "/api/specialty"),
     getAllServices: () => axios.get(this.api + "/api/servicetype"),
