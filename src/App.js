f73916f2329b7c2d6583502940bfd786c12c6f73
Scott Dancer <scottpdancer@gmail.com>
2020-11-17 09:50:28 -0800

    responsive nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React from "react";
+import Home from "./pages/Home"
 import ChefSignup from "./pages/chef-signup"
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div className="App">
+//      <ChefSignup/> 
+//     </div>
+//   );
+// }
+const theme = createMuiTheme ({
+  palette: {
+    primary: {
+      light: '#62727b',
+      main: '#37474f',
+      dark: '#102027',
+      contrastText: '#fafafa',
+    },
+    secondary: {
+      light: '#4f5b62',
+      main: '#263238',
+      dark: '#000a12',
+      contrastText: '#dda300',
+    },
+    contrastThreshold: 3,
+    tonalOffset: 2,
+  },
+});
+
+
 
 function App() {
   return (
-    <div className="App">
-     <ChefSignup/> 
-    </div>
+    <ThemeProvider theme={theme}>
+    <Router>
+      <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/signUp" component={ChefSignup} />
+            {/* <Route exact path="/posts/:id" component={Detail} />
+            <Route component={NoMatch} /> */}
+          </Switch>
+       
+      </div>
+    </Router>
+    </ThemeProvider>
   );
 }
+
+
+
 
 export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./pages/NavBar.css";
+
+function NavBar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <nav>
+        <div className="logo">Logo</div>
+        <div className="header">indulge
+        {/* <p className="tagLine">Savour Opulance</p> */}
+        </div>
+        
+        <ul className="nav-links" style ={{transform: open ? "translateX(0px)" : ""}}>
+          {/* <li>
+            <a>Home</a>
+          </li> */}
+          <li>
+            <a>Guest</a>
+          </li>
+          <li>
+            <a>Chef</a>
+          </li>
+          <li>
+            <a>Search</a>
+          </li>
+        </ul>
+        <i onClick = {() => setOpen(!open)} className="fas fa-bars burgerMenu"></i>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
