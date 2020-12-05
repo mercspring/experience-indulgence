466b16256347900fab9cee2b5d9c48d26805faa4
mercspring <68706508+mercspring@users.noreply.github.com>
2020-12-04 20:08:28 -0800

    changed login modal

--- src/components/SigninModal/index.js
+++ src/components/SigninModal/index.js
@@ -1,12 +1,12 @@
 // React
 import React, { useState } from 'react'
 // Styles and Material UI components
-import { Box, Typography, makeStyles, TextField, Button, Grow } from '@material-ui/core';
+import { Box, Typography, makeStyles, TextField, Button, Grow, Grid, Divider } from '@material-ui/core';
 // API
 import API from '../../utils/API.js'
 
 const useStyles = makeStyles((theme) => ({
-    button:{
+    button: {
         margin: "20px 0"
     },
     box: {
@@ -14,11 +14,15 @@ const useStyles = makeStyles((theme) => ({
     },
     inputText: {
         color: "#f5f5f5"
+    },
+    form: {
+        padding:"1em"
     }
+
 }));
 
 function SigninModal(props) {
-    const [userInfo, setUserInfo] = useState({ username: "", password: "", _id:"" });
+    const [userInfo, setUserInfo] = useState({ username: "", password: "", _id: "" });
     const [validUser, setValidUser] = useState(true)
 
     function onInfoChange(event) {
@@ -29,12 +33,12 @@ function SigninModal(props) {
         }
     }
 
-    function onSubmit(event) {
+    function onSubmitUser(event) {
         event.preventDefault();
         API.login(userInfo).then(res => {
             console.log(res.data);
             localStorage.setItem("userData", JSON.stringify(res.data));
-            setUserInfo({ username: "", password: "", _id:"" });
+            setUserInfo({ username: "", password: "", _id: "" });
             console.log(res.data._id);
             let userId = res.data._id
             props.history.push(`/profile/${userId}`);
@@ -45,7 +49,22 @@ function SigninModal(props) {
             setValidUser(false)
         })
     }
-    
+    function onSubmitChef(event) {
+        event.preventDefault();
+        API.login(userInfo).then(res => {
+            console.log(res.data);
+            localStorage.setItem("userData", JSON.stringify(res.data));
+            setUserInfo({ username: "", password: "", _id: "" });
+            console.log(res.data._id);
+            let userId = res.data._id
+            props.history.push(`/profile/${userId}`);
+            props.setLoggedUser(true);
+            props.handleClose(false);
+            props.setDrawerOpen(false);
+        }).catch(err => {
+            setValidUser(false)
+        })
+    }
     const classes = useStyles();
     return (
         <div>
@@ -54,19 +73,33 @@ function SigninModal(props) {
                     Login
                 </Typography>
             </Box>
-            <form autoComplete="off" onSubmit={onSubmit}>
-                <TextField color="secondary" InputProps={{className : classes.inputText}} fullWidth error={!validUser} label="username" name="username" value={userInfo.username} onChange={onInfoChange} />
-                <TextField color="secondary" InputProps={{className : classes.inputText}} fullWidth error={!validUser} type="password" label="password" name="password" value={userInfo.password} onChange={onInfoChange} />
-                <Button className={classes.button} variant="contained" type="submit" color="primary">Login</Button>
-            </form>
+            <Grid container={true}>
+                <Grid md={5} alignItems="center">
+                    <form autoComplete="off" onSubmit={onSubmitUser} className={classes.form}>
+                        <Typography variant="h6" color="inherit">User Login</Typography>
+                        <TextField color="secondary" InputProps={{ className: classes.inputText }} error={!validUser} label="username" name="username" value={userInfo.username} onChange={onInfoChange} /> <br/>
+                        <TextField color="secondary" InputProps={{ className: classes.inputText }} error={!validUser} type="password" label="password" name="password" value={userInfo.password} onChange={onInfoChange} /> <br/>
+                        <Button className={classes.button} variant="contained" type="submit" color="primary">Login</Button>
+                    </form>
+                </Grid>
+                <Divider orientation="vertical" flexItem />
+                <Grid md={5}>
+                    <form autoComplete="off" onSubmit={onSubmitChef} className={classes.form}>
+                        <Typography variant="h6" color="inherit">Chef Login</Typography>
+                        <TextField color="secondary" InputProps={{ className: classes.inputText }} error={!validUser} label="username" name="username" value={userInfo.username} onChange={onInfoChange} /> <br/>
+                        <TextField color="secondary" InputProps={{ className: classes.inputText }} error={!validUser} type="password" label="password" name="password" value={userInfo.password} onChange={onInfoChange} /> <br/>
+                        <Button className={classes.button} variant="contained" type="submit" color="primary">Login</Button>
+                    </form>
+                </Grid>
+            </Grid>
             <Grow in={!validUser}>
                 <Box p={0.15} mb={0.5} border={1} borderRadius={2} className={classes.box} borderColor="error.main" color="error.main">
                     <Typography variant="body2">
-                        * Incorrect log-in information
+                    * Incorrect log-in information
                     </Typography>
                 </Box>
             </Grow>
-        </div>
+        </div >
     )
 }
 
